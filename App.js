2d3d651cc1961a1d52dd768a21fffb873c0ef530
Diego Bastos <bastosdiegol@gmail.com>
2024-11-19 01:26:54 -0500

    Home Component with Firebase Auth

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "./components/Home";
 import UserManager from "./components/UserManager";
 import { db, firestore, auth } from "./FirebaseConfig";
 
@@ -10,7 +11,8 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="UserManager">
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
         <Stack.Screen
           name="UserManager"
           component={UserManager}
